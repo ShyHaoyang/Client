265e04d3c027229ce2727244633102bd6f97e88c
ShyHaoyangLF_PC <937075931@qq.com>
2020-06-05 17:24:09 +0800

    第一次提交

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public Dictionary<string, string> Clients = new Dictionary<string, string>();
+    private Queue<string> replyQueue = new Queue<string>();
+
+    void Start()
+    {
+        UDPManager.Instance.SetUp("127.0.0.1", 8080, 9090);
+        UDPManager.Instance.Run();
+        UDPManager.Instance.onReply = OnReply;
+
+        HeartBeat();
+    }
+
+    void Update()
+    {
+        if (replyQueue.Count != 0)
+        {
+            Debug.Log(replyQueue.Dequeue());
+        }
+    }
+
+    private void OnDestroy()
+    {
+        UDPManager.Instance.Stop();
+    }
+
+    void OnReply(byte[] _reply, string _address, int _port)
+    {
+        string reply = System.Text.Encoding.UTF8.GetString(_reply);
+        replyQueue.Enqueue(reply);
+    }
+
+    private void HeartBeat()
+    {
+        StartCoroutine(heartBeat());
+    }
+
+    IEnumerator heartBeat()
+    {
+        while (true)
+        {
+            UDPManager.Instance.Report("ping:127.0.0.1:8080");
+            yield return new WaitForSeconds(1f);
+        }
+
+    }
+}
+
